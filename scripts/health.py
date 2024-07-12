e0e0a744a56d3d35fc34bf2597565371182c8a25
cjber <cjberragan@gmail.com>
2024-07-12 11:49:34 +0100

    update demo script

--- scripts/health.py
+++ scripts/health.py
@@ -1,25 +1,17 @@
 import cudf
-import cugraph
 import geopandas as gpd
-import numpy as np
 import pandas as pd
 
 from ukroutes import Routing
 from ukroutes.common.utils import Paths
-from ukroutes.preprocessing import process_os
 from ukroutes.process_routing import add_to_graph, add_topk
+from ukroutes.oproad.utils import process_oproad
 
+# process oproad nodes and edges
+nodes, edges = process_oproad(outdir=Paths.OS_GRAPH)  # or outdir=None
+
+# read in health dataa and postcodes
 health = pd.read_parquet("./data/processed/health.parquet").dropna().sample(1000)
-
-nodes: cudf.DataFrame = cudf.from_pandas(
-    pd.read_parquet(Paths.OS_GRAPH / "nodes.parquet")
-)
-edges: cudf.DataFrame = cudf.from_pandas(
-    pd.read_parquet(Paths.OS_GRAPH / "edges.parquet")
-)
-
-health, nodes, edges = add_to_graph(health, nodes, edges, 1)
-
 postcodes = pd.read_csv(
     "./data/raw/onspd/ONSPD_FEB_2024.csv",
     usecols=["PCD", "OSEAST1M", "OSNRTH1M", "DOTERM", "CTRY"],
@@ -34,22 +26,27 @@ postcodes = (
     .dropna()
 )
 
-postcodes, nodes, edges = add_to_graph(postcodes, nodes, edges, 1)
-health = add_topk(health, postcodes, 10)
+# add health and postcodes to road network
+health, nodes, edges = add_to_graph(health, nodes, edges, 10)
+postcodes, nodes, edges = add_to_graph(postcodes, nodes, edges, 10)
 
+# find the top 10 closest health facilities to each postcode
+health = add_topk(health, postcodes, 25)
+
+# run the routing class
 routing = Routing(
-    name="health",
     edges=edges,
     nodes=nodes,
-    outputs=postcodes,
     inputs=health,
+    outputs=postcodes,
     weights="time_weighted",
     min_buffer=5000,
     max_buffer=500_000,
-    # cutoff=300,
+    cutoff=300,
 )
 routing.fit()
 
+# join distances to postcodes
 distances = (
     routing.distances.set_index("vertex")
     .join(cudf.from_pandas(postcodes).set_index("node_id"), how="right")
@@ -57,15 +54,12 @@ distances = (
     .to_pandas()
 )
 
-# OUT_FILE = Paths.OUT_DATA / "distances_health.csv"
-# distances.to_csv(OUT_FILE, index=False)
+import matplotlib.pyplot as plt
 
 distances = gpd.GeoDataFrame(
     distances, geometry=gpd.points_from_xy(distances.easting, distances.northing)
 )
 distances.reset_index().sort_values("distance").plot(column="distance")
-distances.sort_values("distance").tail(25)
 
-import matplotlib.pyplot as plt
 
-plt.show()
+plt.savefig("./figs/health_example.png")
